976110ce5d8b3a98d2edce2185bf38ea84bc42b6
oyama <oyama@projectcodex.co>
2015-09-21 15:17:51 +0200

    data route for products

--- routes/products.js
+++ routes/products.js
@@ -1,3 +1,8 @@
+/***
+ * A very basic CRUD example using MySQL
+ */	
+//todo - fix the error handling
+
 exports.show = function (req, res, next) {
 	req.getConnection(function(err, connection){
 		if (err) 
@@ -17,13 +22,13 @@ exports.add = function (req, res, next) {
 
 		var input = JSON.parse(JSON.stringify(req.body));
 		var data = {
-            description : input.description,
+            Product_name : input.Product_name,
         };
 
 		if (err) 
 			return next(err);
 		
-		connection.query('insert into products set ?', data, function(err, results) {
+		connection.query('insert into Products_td set ?', data, function(err, results) {
         	if (err)
               console.log("Error inserting : %s ",err );
          
@@ -36,12 +41,12 @@ exports.get = function(req, res, next){
 	var id = req.params.id;
 	req.getConnection(function(err, connection){
 
-		connection.query('SELECT * FROM products WHERE id = ?', [id], function(err,rows){
+		connection.query('SELECT * FROM Products_td WHERE id = ?', [id], function(err,rows){
 			if(err){
     			console.log("Error Selecting : %s ",err );
 			}
 
-			res.render('edit',{page_title:"Edit Customers - Node.js", data : rows[0]});      
+			res.render('editProduct',{page_title:"Edit Customers - Node.js", data : rows[0]});      
 		}); 
 	});
 };
@@ -51,7 +56,7 @@ exports.update = function(req, res, next){
 	var data = JSON.parse(JSON.stringify(req.body));
     var id = req.params.id;
     req.getConnection(function(err, connection){
-    	connection.query('UPDATE products SET ? WHERE id = ?', [data, id], function(err, rows){
+    	connection.query('UPDATE Products_td SET ? WHERE id = ?', [data, id], function(err, rows){
     		
     		if (err){
               console.log("Error Updating : %s ",err );
@@ -65,7 +70,7 @@ exports.delete = function(req, res, next){
 	var id = req.params.id;
 	req.getConnection(function(err, connection){
 
-		connection.query('DELETE FROM products WHERE id = ?', [id], function(err,rows){
+		connection.query('DELETE FROM Products_td WHERE id = ?', [id], function(err,rows){
 			if(err){
     			console.log("Error Selecting : %s ",err );
 			}
@@ -74,3 +79,4 @@ exports.delete = function(req, res, next){
 		 
 	});
 };
+
