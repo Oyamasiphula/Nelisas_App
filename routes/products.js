df1cb06b8f08626845f9afa9f2e6458e1c9db84c
oyama <oyama@projectcodex.co>
2015-10-07 14:37:53 +0200

    now done categories on products table

--- routes/products.js
+++ routes/products.js
@@ -137,7 +137,7 @@ exports.showPopularProduct = function(req, res, next){
 		
 		connection.query('SELECT SUM(qTy) AS Quantity, Product_id ,Product_name FROM Sales_td INNER JOIN Products_td ON Sales_td.Product_id = Products_td.id GROUP BY Product_name ORDER BY SUM(qTy) DESC LIMIT 0,1', [], function(err, results){
 		if (err) 
-					return next(err);
+				return next(err);
     	
     		res.render( 'products', {
     			mostPopularProduct : results
