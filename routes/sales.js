eda1a20f4e1817788e57335648641d8e3a62e151
oyama <oyama@projectcodex.co>
2015-10-08 17:28:19 +0200

    added new sales route and its correctly working nice!

--- routes/sales.js
+++ routes/sales.js
@@ -1,26 +1,56 @@
-
 exports.show = function(req, res, next){
 			
-			var id = req.params.id;
-			var data = JSON.parse(JSON.stringify(req.body));
+	var id = req.params.id;
+	var data = JSON.parse(JSON.stringify(req.body));
 
 	req.getConnection(function(err,connection){
 
-		connection.query('SELECT Sales_td.Product_id, SUM(product_price), Product_name FROM Sales_td INNER JOIN Products_td ON Sales_td.Product_id = Products_td.id group by Product_id', [],function(err,results){
-						if(err)
-							return next("error selecting : %s ", err)
-						console.log(results);
+		connection.query('SELECT Sales_td.Product_id, SUM(Sales_td.qTy * Sales_td.product_price) AS Tot_Earnings_Per_Product, Product_name FROM Sales_td INNER JOIN Products_td ON Sales_td.Product_id = Products_td.id INNER JOIN Categories_td ON Products_td.Category_id = Categories_td.id group by Product_id', [],function(err,results){
+			if(err)
+					return next("error selecting : %s ", err);
 
-				res.render('sales' ,{
-					Sales : results
-				});			
+					res.render('sales' ,{
+						Sales : results
+					});			
+				});
 
 			});
 
-		});
+	}
 
-	};
-// SELECT [1st_table_name].id, [table_column], [table_column] 
-// FROM [1st_table_name] INNER JOIN 
-// [another_table_name] ON [1st_table_name].[match_colms_by_vals] 
-// = [another_table_name].[match_colms_by_vals]
+	exports.showCategories = function(req, res, next){
+		var id = req.params.id;
+		var data = JSON.parse(JSON.stringify(req.body));
+
+			req.getConnection(function(err ,connection){
+				
+				connection.query('SELECT Categories_td.Category_name, SUM(Sales_td.qTy * Sales_td.product_price) AS Tot_Earnings_Per_Product, Product_name FROM Sales_td INNER JOIN Products_td ON Sales_td.Product_id = Products_td.id INNER JOIN Categories_td ON Products_td.Category_id = Categories_td.id group by Products_td.Product_name',[] ,function(err, EarningsPerProductsCategory){
+				if(err)
+						return next("error selecting : %s " ,err)
+				
+						res.render('salesSummary', {
+							sumOfEarnings : EarningsPerProductsCategory
+					})
+				})
+
+			})
+
+	}
+	exports.EarningsPerCategory = function(req, res, next){
+		var id = req.params.id;
+		var data = JSON.parse(JSON.stringify(req.body));
+
+			req.getConnection(function(err ,connection){
+				
+				connection.query('SELECT Categories_td.Category_name, SUM(Sales_td.qTy * Sales_td.product_price) AS Tot_Earnings_Per_Product FROM Sales_td INNER JOIN Products_td ON Sales_td.Product_id = Products_td.id INNER JOIN Categories_td ON Products_td.Category_id = Categories_td.id group by Products_td.Category_id',[] ,function(err, EarningsPerCategoryData){
+				if(err)
+						return next("error selecting : %s " ,err)
+				
+						res.render('salesEarningsPerCat', {
+							sumOfEarningsPerCategory : EarningsPerCategoryData
+						})
+				})
+
+			})
+
+	}
