b3979be38a8be9e001ce81286bb45edd4709e112
oyama <oyama@projectcodex.co>
2015-10-23 16:13:43 +0200

    update but still have to fix sales search text box

--- public/js/search.js
+++ public/js/search.js
@@ -1,21 +1,30 @@
-//Using Jquery
+//Jquery AKA AJAX searching patern
 
 $(document).ready(function(){
 //Products Search
-$("#productPullReq").keyup(function(){
-	var productPullReq = $("#productPullReq").val();
-	$.get("/products/search/" + productPullReq, function(results){
-		$("#reqProductsOutPut").html(results);
+	$("#productPullReq").keyup(function(){
+		var productPullReq = $("#productPullReq").val();
+		$.get("/products/search/" + productPullReq, function(results){
+			$("#reqProductsOutPut").html(results);
 		});
 	});	
 
-
-//Category Search
-$("#pullCatNameReq").keyup(function(){
-	var pullCatNameReq = $("#pullCatNameReq").val();
-	$.get("/productsCategories/search/" + pullCatNameReq, function(results){
-		$("#reqCategoriesOutPut").html(results);
-		console.log(results);
+					//Category Search
+		$("#pullCatNameReq").keyup(function(){
+		var pullCatNameReq = $("#pullCatNameReq").val();
+			$.get("/productsCategories/search/" + pullCatNameReq, function(results){
+				$("#reqCategoriesOutPut").html(results);
+				
+			});
 		});
-	});
+				// sales search
+		$("#pullProductsEarnings").keyup(function(){
+		var pullProductsEarnings = $("pullProductsEarnings").val();
+			$.get("/sales/search/" + pullProductsEarnings, function(results){
+				$("#reqSumOfProductsEarningsOutPut").html(results);
+
+			})
+		})
+
 });	
+
