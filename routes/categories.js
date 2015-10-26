16bfd111340e881eda641c038d1d7befbf882a91
oyama <oyama@projectcodex.co>
2015-10-26 11:33:14 +0200

    update

--- routes/categories.js
+++ routes/categories.js
@@ -11,7 +11,7 @@ exports.searchCategories = function(req, res, next){
 
         var CategoryName = 'SELECT Category_name from Categories_td WHERE Category_name LIKE ?';
         
-        connection.query(CategoryName, [pullCatNameReq, pullCatNameReq],function(err, catNameAsResult){
+        connection.query(CategoryName,[pullCatNameReq, pullCatNameReq],function(err, catNameAsResult){
 
                 res.render('searchCategories',{
                     categoryName : catNameAsResult,
