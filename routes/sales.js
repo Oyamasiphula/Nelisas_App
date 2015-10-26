f137ccb9a0761507d010f9aed7fe6a54970a4e08
oyama <oyama@projectcodex.co>
2015-10-26 11:37:11 +0200

    working but not refreshing still need to be fixed

--- routes/sales.js
+++ routes/sales.js
@@ -4,19 +4,17 @@ exports.searchSales = function(req, res, next){
 			var pullProductsEarnings = req.params.query;
 				pullProductsEarnings = '%' + pullProductsEarnings + '%';
 
-		connection.query('SELECT id,Product_name, Category_name FROM (SELECT Products_td.id,Products_td.Product_name, Categories_td.Category_name, Products_td.Category_id FROM Products_td, Categories_td where Products_td.Category_id = Categories_td.id) AS prods_cats WHERE Product_name LIKE ? OR Category_name LIKE ?',[pullProductsEarnings, pullProductsEarnings],function(err, productsEarningsResults){
-					console.log(productsEarningsResults)
+		connection.query('SELECT Sales_td.Product_id, SUM( Sales_td.qTy * Sales_td.product_price ) AS Tot_Earnings_Per_Product, Product_name FROM Sales_td INNER JOIN Products_td ON Sales_td.Product_id = Products_td.id INNER JOIN Categories_td ON Products_td.Category_id = Categories_td.id GROUP BY Products_td.Product_name LIKE ? LIMIT 0 , 30',[pullProductsEarnings, pullProductsEarnings],function(err, results){
 				if(err)
-						return next("error selecting : %s ", err)
+					return next("error selecting : %s ", err)
 
-
-			res.render('searchProductsEarnings',{
-				findProductNames : productsEarningsResults,
-				layout : false
+				res.render('searchProductsEarnings',{
+					Sales : results,
+					layout : false
+				})
 			})
-		})
 
-	})
+		})
 
 }
 
@@ -29,15 +27,15 @@ exports.show = function(req, res, next){
 
 		connection.query('SELECT Sales_td.Product_id, SUM(Sales_td.qTy * Sales_td.product_price) AS Tot_Earnings_Per_Product, Product_name FROM Sales_td INNER JOIN Products_td ON Sales_td.Product_id = Products_td.id INNER JOIN Categories_td ON Products_td.Category_id = Categories_td.id group by Product_id', [],function(err,results){
 			if(err)
-					return next("error selecting : %s ", err);
+				return next("error selecting : %s ", err);
 
-					res.render('sales' ,{
-						Sales : results
-					});			
-				});
+				res.render('sales' ,{
+					Sales : results
+				});			
 			});
+		});
 
-	}
+}
 
 	exports.showCategories = function(req, res, next){
 		var id = req.params.id;
@@ -47,16 +45,16 @@ exports.show = function(req, res, next){
 				
 				connection.query('SELECT Categories_td.Category_name, SUM(Sales_td.qTy * Sales_td.product_price) AS Tot_Earnings_Per_Product, Product_name FROM Sales_td INNER JOIN Products_td ON Sales_td.Product_id = Products_td.id INNER JOIN Categories_td ON Products_td.Category_id = Categories_td.id group by Products_td.Product_name',[] ,function(err, EarningsPerProductsCategory){
 				if(err)
-						return next("error selecting : %s " ,err)
+					return next("error selecting : %s " ,err)
 				
-						res.render('salesSummary', {
-							sumOfEarnings : EarningsPerProductsCategory
-					})
+					res.render('salesSummary', {
+						sumOfEarnings : EarningsPerProductsCategory
 				})
-
 			})
 
-	}
+		})
+
+}
 	exports.EarningsPerCategory = function(req, res, next){
 		var id = req.params.id;
 		var data = JSON.parse(JSON.stringify(req.body));
@@ -65,13 +63,13 @@ exports.show = function(req, res, next){
 				
 				connection.query('SELECT Categories_td.Category_name, SUM(Sales_td.qTy * Sales_td.product_price) AS Tot_Earnings_Per_Product FROM Sales_td INNER JOIN Products_td ON Sales_td.Product_id = Products_td.id INNER JOIN Categories_td ON Products_td.Category_id = Categories_td.id group by Products_td.Category_id',[] ,function(err, EarningsPerCategoryData){
 				if(err)
-						return next("error selecting : %s " ,err)
+					return next("error selecting : %s " ,err)
 				
-						res.render('salesEarningsPerCat', {
-							sumOfEarningsPerCategory : EarningsPerCategoryData
-						})
-				})
-
+					res.render('salesEarningsPerCat', {
+						sumOfEarningsPerCategory : EarningsPerCategoryData
+					})
 			})
 
-	}
+		})
+
+}
