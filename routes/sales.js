7c23a0ce6b88d7ca0fb979da0e31cbbc66044308
oyama <oyama@projectcodex.co>
2015-10-26 16:25:56 +0200

    textbox for a search feature is working but not refreshing still need to be fixed

--- routes/sales.js
+++ routes/sales.js
@@ -1,10 +1,11 @@
 exports.searchSales = function(req, res, next){
 	req.getConnection(function(err, connection){
 
-			var pullProductsEarnings = req.params.query;
+			var pullProductsEarnings = req.params.query,
 				pullProductsEarnings = '%' + pullProductsEarnings + '%';
 
-		connection.query('SELECT Sales_td.Product_id, SUM( Sales_td.qTy * Sales_td.product_price ) AS Tot_Earnings_Per_Product, Product_name FROM Sales_td INNER JOIN Products_td ON Sales_td.Product_id = Products_td.id INNER JOIN Categories_td ON Products_td.Category_id = Categories_td.id GROUP BY Products_td.Product_name LIKE ? LIMIT 0 , 30',[pullProductsEarnings, pullProductsEarnings],function(err, results){
+		connection.query(' SELECT Sales_td.Product_id ,Products_td.Product_name, SUM(Sales_td.qTy * Sales_td.product_price) AS Tot_Earnings_Per_Product, Product_name FROM Sales_td INNER JOIN Products_td ON Sales_td.Product_id = Products_td.id WHERE Products_td.Product_name LIKE ?',[pullProductsEarnings, pullProductsEarnings],function(err, results){
+				console.log(results)
 				if(err)
 					return next("error selecting : %s ", err)
 
