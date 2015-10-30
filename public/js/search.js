2f0ca36759909371981bc76c549196817fa412b4
oyama <oyama@projectcodex.co>
2015-10-30 12:04:05 +0200

    :) all sales queries are now on a good state ('working')

--- public/js/search.js
+++ public/js/search.js
@@ -23,25 +23,49 @@ $(document).ready(function(){
 			$("#reqProductsEarning").html(results);
 		});
 			
-			if (pullProductsEarnings === true) {
-				$.get("/sales/search/" + pullProductsEarnings, function(results){
+				if (pullProductsEarnings === true) {
+		$.get("/sales/search/" + pullProductsEarnings, function(results){
 					$("#reqSumOfProductsEarning").html(results);
-				});
+		});
 
 				} else if(pullProductsEarnings === "" || false){
 					window.location.replace("http://localhost:2001/sales");
 
-			      };
+				   };
 
 	});
 
 	$("#pullSumOfEarnings").keyup(function(){
 		var pullSumOfEarnings = $("#pullSumOfEarnings").val();
-			$.get("/salesSummary/search/" + pullSumOfEarnings, function(results){
+			$.get("/salesSummary/showCategories/search/" + pullSumOfEarnings, function(results){
 				$("#sumOfProductsEarningsOutPut").html(results);
 			});
+			if (pullSumOfEarnings === true) {
+		$.get("/salesSummary/EarningsPerCategory/search/" + pullSumOfEarnings, function(results){
+					$("#reqEarningsCategoryNameTd").html(results);
+		});
 
+				} else if(pullSumOfEarnings === "" || false){
+					window.location.replace("http://localhost:2001/salesSummary/showCategories");
+
+			};
 	});
 
+	$('#pullCategoryName').keyup(function(){
+		var pullCategoryName = $('#pullCategoryName').val();
+		$.get('/salesSummary/EarningsPerCategory/search/' + pullCategoryName, function(results){
+				$('#reqEarningsCategoryNameTd').html(results);
+
+		})
+		if (pullCategoryName === true) {
+		$.get("/salesSummary/EarningsPerCategory/search/" + pullCategoryName, function(results){
+					$("#reqEarningsCategoryNameTd").html(results);
+		});
+
+				} else if(pullCategoryName === "" || false){
+					window.location.replace("http://localhost:2001/salesSummary/EarningsPerCategory");
+
+				   };
+	})
 });	
 
