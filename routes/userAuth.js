e1a864add3d79a588bbd09fb08b457a49137d0a6
oyamasiphula <oyama@projectcodex.co>
2016-08-09 14:50:29 +0200

    added signin and signup functions

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -1,3 +1,43 @@
+exports.sign_In = function(req, res){
+  res.render('sign_In', { message: req.flash('loginMessage')})
+}
+
+exports.sign_up = function(req, res){
+  res.render('sign_up', { message: req.flash('signupMessage') })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 exports.checkUser = function(req, res, next){
   if (req.session.user){
       // calling the next function - just proceeds to the route.
