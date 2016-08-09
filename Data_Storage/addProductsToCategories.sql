18b3b0c61e5903819f442535ea1a0ba95ba82150
oyamasiphula <oyama@projectcodex.co>
2016-08-09 16:41:42 +0200

    removed all sql in this current dir - no longer neDDDed this

--- Data_Storage/addProductsToCategories.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE Products_td ADD CONSTRAINT fk_Category_id FOREIGN KEY (Category_id) REFERENCES Categories_td(id);
--- Data_Storage/suppliers.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO Suppliers_td(Supplier_name)
-SELECT DISTINCT shop FROM stock_purchases_csv;
