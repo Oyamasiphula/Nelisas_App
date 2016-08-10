4dae4250f6837c475fd770d099a1e78759d89bb4
oyamasiphula <oyama@projectcodex.co>
2016-08-10 09:11:53 +0200

    added passport(mysql query functions for sign up and login form)

---
+++ Data_Storage/config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'projectcodex'
+    },
+	'database': 'Nels_db',
+    'users_table': 'users'
+};
