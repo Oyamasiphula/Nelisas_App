77480369154a3120ff023a7336033495e3ed9cc4
oyamasiphula <oyama@projectcodex.co>
2016-08-10 09:11:52 +0200

    added more finctions

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -1,3 +1,5 @@
+var flash = require('connect-flash');
+
 exports.sign_In = function(req, res){
   res.render('sign_In', { message: req.flash('loginMessage')})
 }
@@ -6,43 +8,32 @@ exports.sign_up = function(req, res){
   res.render('sign_up', { message: req.flash('signupMessage') })
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-exports.checkUser = function(req, res, next){
-  if (req.session.user){
-      // calling the next function - just proceeds to the route.
-      return next();
-  }
-  // the user is not logged in redirect them to the login page
-  res.redirect('login');
+exports.verifyUser = function(req, res) {
+  res.render('profile', {
+    user : req.user // get the user out of session and pass to template
+  });
 };
+
+// route middleware to make sure
+exports.isLoggedIn = function(req, res, next) {
+	// if user is authenticated in the session, carry on
+	if (req.isAuthenticated())
+		return next();
+
+	// if they aren't redirect them to the home page
+	res.redirect('/');
+}
+
+exports.logout = function(req, res) {
+  req.logout();
+  res.redirect('/');
+};
+//
+// exports.checkUser = function(req, res, next){
+//   if (req.session.user){
+//       // calling the next function - just proceeds to the route.
+//       return next();
+//   }
+//   // the user is not logged in redirect them to the login page
+//   res.redirect('login');
+// };
