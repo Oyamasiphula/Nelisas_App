15a6347027c644049bf9f5e76b21e47237f3e6bd
oyamasiphula <oyama@projectcodex.co>
2016-08-11 11:11:29 +0200

    added connection pool for supppliers

---
+++ routes/suppliers.js
@@ -0,0 +1,16 @@
+exports.show = function(req, res, next){
+
+  req.getConnection(function(err, connection){
+
+      connection.query('select * from Suppliers_td',[],function(err,result){
+        if (err)
+                return next("Error Selecting : %s ",err );
+            res.render('suppliers',{
+                suppliers : result
+          })
+      })
+
+  })
+
+
+}
