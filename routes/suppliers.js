2934880d2d5be29115886380eaf6e608a286f731
oyamasiphula <oyama@projectcodex.co>
2016-08-12 13:09:07 +0200

    Beautified syntax and no line spacing-(cleaned)

--- routes/suppliers.js
+++ routes/suppliers.js
@@ -1,16 +1,11 @@
-exports.show = function(req, res, next){
-
-  req.getConnection(function(err, connection){
-
-      connection.query('select * from Suppliers_td',[],function(err,result){
-        if (err)
-                return next("Error Selecting : %s ",err );
-            res.render('suppliers',{
-                suppliers : result
-          })
-      })
-
-  })
-
-
-}
+exports.show = function(req, res, next) {
+  req.getConnection(function(err, connection) {
+    connection.query('select * from Suppliers_td', [], function(err, result) {
+      if (err)
+          return next("Error Selecting : %s ", err);
+      res.render('suppliers', {
+        suppliers: result
+      });
+    });
+  });
+};
