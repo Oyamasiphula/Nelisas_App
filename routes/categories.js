45aeadcca6dc7d68608360b1914b3dca53df9dd4
oyamasiphula <oyama@projectcodex.co>
2016-08-12 13:09:06 +0200

    Beautified syntax and no line spacing-(cleaned)

--- routes/categories.js
+++ routes/categories.js
@@ -1,120 +1,105 @@
-/***
- * A very basic CRUD example using MySQL
- */ 
-//todo - fix the error handling
-exports.searchCategories = function(req, res, next){
+exports.searchCategories = function(req, res, next) {
+  var pullCatNameReq = req.params.query;
+      pullCatNameReq = '%' + pullCatNameReq + '%';
+  var CategoryName = 'SELECT Category_name from Categories_td WHERE Category_name LIKE ?';
 
-    req.getConnection(function(err, connection){
+  req.getConnection(function(err, connection) {
+    connection.query(CategoryName, [pullCatNameReq, pullCatNameReq], function(err, catNameAsResult) {
 
-        var pullCatNameReq = req.params.query;
-            pullCatNameReq = '%' + pullCatNameReq + '%';
-
-        var CategoryName = 'SELECT Category_name from Categories_td WHERE Category_name LIKE ?';
-        
-        connection.query(CategoryName,[pullCatNameReq, pullCatNameReq],function(err, catNameAsResult){
-
-                res.render('searchCategories',{
-                    categoryName : catNameAsResult,
-                    layout : false
-                });
-
-        });
-
+      res.render('searchCategories', {
+        categoryName: catNameAsResult,
+        layout: false
+      });
     });
-
+  });
 };
 
-exports.show = function (req, res, next) {
-    req.getConnection(function(err, connection){
+exports.show = function(req, res, next) {
+  var showMostPopularCat = 'SELECT Categories_td.Category_name ,SUM(Sales_td.qTy) AS Quantity FROM Sales_td INNER JOIN Products_td ON Sales_td.Product_id = Products_td.id INNER JOIN Categories_td ON Products_td.Category_id = Categories_td.id GROUP BY Categories_td.Category_name ORDER BY Quantity DESC LIMIT 0,1';
+  var showLeastPopularCAt = 'SELECT Categories_td.Category_name ,SUM(Sales_td.qTy) AS Quantity FROM Sales_td INNER JOIN Products_td ON Sales_td.Product_id = Products_td.id INNER JOIN Categories_td ON Products_td.Category_id = Categories_td.id GROUP BY Categories_td.Category_name ORDER BY Quantity ASC LIMIT 0,1';
 
-    var showMostPopularCat = 'SELECT Categories_td.Category_name ,SUM(Sales_td.qTy) AS Quantity FROM Sales_td INNER JOIN Products_td ON Sales_td.Product_id = Products_td.id INNER JOIN Categories_td ON Products_td.Category_id = Categories_td.id GROUP BY Categories_td.Category_name ORDER BY Quantity DESC LIMIT 0,1';
-        
-    var showLeastPopularCAt = 'SELECT Categories_td.Category_name ,SUM(Sales_td.qTy) AS Quantity FROM Sales_td INNER JOIN Products_td ON Sales_td.Product_id = Products_td.id INNER JOIN Categories_td ON Products_td.Category_id = Categories_td.id GROUP BY Categories_td.Category_name ORDER BY Quantity ASC LIMIT 0,1';    
-    
-        connection.query(showMostPopularCat, [], function(err, showMostPopularCat){
-            if(err)
-                return next(err)
-        connection.query(showLeastPopularCAt, [], function(err, showLeastPopularCAt){
-            if(err){
-                return next(err)
-            }
+  req.getConnection(function(err, connection) {
+    connection.query(showMostPopularCat, [], function(err, showMostPopularCat) {
+      if (err)
+        return next(err)
+      connection.query(showLeastPopularCAt, [], function(err, showLeastPopularCAt) {
+        if (err) {
+          return next(err)
+        }
         connection.query('SELECT * from Categories_td', [], function(err, results) {
-            if (err){ 
-                return next(err);
-            }
-            res.render( 'productsCategories',{
-                categories : results,
-                showMostPopularCat : showMostPopularCat,
-                showLeastPopularCAt : showLeastPopularCAt
-                       
-                    });
-                 });
-            });
+          if (err) {
+            return next(err);
+          }
+          res.render('productsCategories', {
+            categories: results,
+            showMostPopularCat: showMostPopularCat,
+            showLeastPopularCAt: showLeastPopularCAt
+          });
         });
-     });
-};
-
-exports.showAdd = function(req, res, next){
-    res.render('addProductsCategories');
-};
-
-exports.add = function (req, res, next) {
-    req.getConnection(function(err, connection){
-        
-        var input = JSON.parse(JSON.stringify(req.body));
-        
-        var data = { Category_name : input.Category_name };
-           
-        connection.query('insert into Categories_td set ?', data, function(err, results) {
-            if (err){
-                    return next(err);
-            }
-
-            res.redirect('/productsCategories');
-        });
+      });
     });
+  });
 };
+exports.showAdd = function(req, res, next) {
+  res.render('addProductsCategories');
+};
+exports.add = function(req, res, next) {
+  var input = JSON.parse(JSON.stringify(req.body));
+  var data = {
+    Category_name: input.Category_name
+  };
 
-exports.get = function(req, res, next){
-        var id = req.params.id;
-    req.getConnection(function(err, connection){
+  req.getConnection(function(err, connection) {
+    connection.query('insert into Categories_td set ?', data, function(err, results) {
+      if (err) {
+        return next(err);
+      }
 
-        connection.query('SELECT * FROM Categories_td WHERE id = ?', [id], function(err,rows){
-            if(err){
-                        return next(err);
-            }
-
-            res.render('editProductsCategories',{page_title:"Edit Customers - Node.js", data : rows[0]});      
-        }); 
+      res.redirect('/productsCategories');
     });
+  });
 };
 
-exports.update = function(req, res, next){
+exports.get = function(req, res, next) {
+  var id = req.params.id;
+  req.getConnection(function(err, connection) {
 
-        var data = JSON.parse(JSON.stringify(req.body));
-        var id = req.params.id;
-    req.getConnection(function(err, connection){
-        connection.query('UPDATE Categories_td SET ? WHERE id = ?', [data, id], function(err, rows){
-            
-            if (err){
-              return next(err);
-            }
-            res.redirect('/productsCategories');
-        });
+    connection.query('SELECT * FROM Categories_td WHERE id = ?', [id], function(err, rows) {
+      if (err) {
+        return next(err);
+      }
+
+      res.render('editProductsCategories', {
+        page_title: "Edit Customers - Node.js",
+        data: rows[0]
+      });
     });
+  });
 };
 
-exports.delete = function(req, res, next){
-    var id = req.params.id;
-    req.getConnection(function(err, connection){
+exports.update = function(req, res, next) {
+  var data = JSON.parse(JSON.stringify(req.body));
+  var id = req.params.id;
 
-        connection.query('DELETE FROM Categories_td WHERE id = ?', [id], function(err,rows){
-            if(err){
-                    return next(err);
-            }
-            res.redirect('/productsCategories');
-        });
-         
+  req.getConnection(function(err, connection) {
+    connection.query('UPDATE Categories_td SET ? WHERE id = ?', [data, id], function(err, rows) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect('/productsCategories');
     });
+  });
 };
 
+exports.delete = function(req, res, next) {
+  var id = req.params.id;
+
+  req.getConnection(function(err, connection) {
+    connection.query('DELETE FROM Categories_td WHERE id = ?', [id], function(err, rows) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect('/productsCategories');
+    });
+  });
+};
