a764145d3818829e6b6b6814c1b63392b200f1ca
oyamasiphula <oyama@projectcodex.co>
2016-08-15 00:46:41 +0200

    added add supplier and show addsupplier template functions

--- routes/suppliers.js
+++ routes/suppliers.js
@@ -2,10 +2,40 @@ exports.show = function(req, res, next) {
   req.getConnection(function(err, connection) {
     connection.query('select * from Suppliers_td', [], function(err, result) {
       if (err)
-          return next("Error Selecting : %s ", err);
+        return next("Error Selecting : %s ", err);
       res.render('suppliers', {
         suppliers: result
       });
     });
   });
 };
+
+exports.showaddSupplier = function(req, res, next) {
+  req.getConnection(function(err, connection) {
+    connection.query('SELECT * from Suppliers_td', [], function(err, results) {
+      if (err)
+        return next("Error Selecting : %s ", err);
+
+      res.render('addSuppliers', {
+        suppliers: results
+      });
+    });
+  });
+}
+
+
+exports.addSupplier = function(req, res, next) {
+  var input = JSON.parse(JSON.stringify(req.body));
+  var data = {
+		Supplier_name: input.Supplier_name,
+	};
+
+  req.getConnection(function(err, connection) {
+    connection.query('insert into Suppliers_td set ?', data, function(err, result) {
+      if (err)
+        return next("Error inserting : %s ", err);
+
+      res.redirect('/suppliers');
+    });
+  });
+}
