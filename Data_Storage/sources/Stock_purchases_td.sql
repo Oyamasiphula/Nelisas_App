2ab805898bd415d5373b9303cd0fc754cd89d35c
oyamasiphula <oyama@projectcodex.co>
2016-09-21 11:13:15 +0200

    removed outdated databackup

--- Data_Storage/sources/Stock_purchases_td.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO stock_purchases(qTy,product_price,Sales_date,Product_id)
-SELECT sales_csv.no_sold,sales_csv.sales_price,sales_csv.date, Products_td.id
-FROM sales_csv 
-INNER JOIN Products_td
-ON Products_td.Product_name = sales_csv.stock_item;
