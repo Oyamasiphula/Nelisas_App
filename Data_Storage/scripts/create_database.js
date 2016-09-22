27e51a6ee1219b264e2e21ecc4e40e48eff50e82
oyamasiphula <oyama@projectcodex.co>
2016-09-22 13:51:52 +0200

    data backup update

--- Data_Storage/scripts/create_database.js
+++ Data_Storage/scripts/create_database.js
@@ -13,7 +13,7 @@ CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.users_table + '` ( \
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
     `username` VARCHAR(20) NOT NULL, \
     `password` CHAR(60) NOT NULL, \
-        PRIMARY KEY (`id`), \
+    PRIMARY KEY (`id`), \
     UNIQUE INDEX `id_UNIQUE` (`id` ASC), \
     UNIQUE INDEX `username_UNIQUE` (`username` ASC) \
 )');
