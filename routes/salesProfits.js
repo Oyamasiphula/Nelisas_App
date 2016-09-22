fc862619ef64c9444cf77e4851fb4d318822cead
oyamasiphula <oyama@projectcodex.co>
2016-09-22 13:56:10 +0200

    connection pool fix

--- routes/salesProfits.js
+++ routes/salesProfits.js
@@ -3,10 +3,10 @@ exports.show = function(req, res, next) {
 		  var data = JSON.parse(JSON.stringify(req.body));
 
   req.getConnection(function(err, connection) {
-    connection.query('SELECT Products_td.id,Products_td.Product_name, (SELECT SUM(Sales_td.qTy * Sales_td.product_price) FROM Sales_td WHERE Product_id = Products_td.id)-(SELECT SUM(Purchases_td.qTy * Purchases_td.product_price) FROM Purchases_td WHERE Product_id = Products_td.id) AS Profits FROM Products_td GROUP BY Profits DESC', function(err, result) {
+    connection.query('SELECT Products_td.Product_id,Products_td.Product_name, (SELECT SUM(Sales_td.qTy * Sales_td.product_price) FROM Sales_td WHERE Product_id = Products_td.Product_id)-(SELECT SUM(Purchases_td.qTy * Purchases_td.product_price) FROM Purchases_td WHERE Product_id = Products_td.Product_id) AS Profits FROM Products_td GROUP BY Product_id, Profits DESC', function(err, result) {
       if (err)
         return next("error selecting : %s", err)
-      connection.query('SELECT Products_td.id,Products_td.Product_name, (SELECT SUM(Sales_td.qTy * Sales_td.product_price) FROM Sales_td WHERE Product_id = Products_td.id)-(SELECT SUM(Purchases_td.qTy * Purchases_td.product_price) FROM Purchases_td WHERE Product_id = Products_td.id) AS Profits FROM Products_td GROUP BY Profits DESC', function(err, showMostProfitableProduct) {
+      connection.query('SELECT Products_td.Product_id,Products_td.Product_name, (SELECT SUM(Sales_td.qTy * Sales_td.product_price) FROM Sales_td WHERE Product_id = Products_td.Product_id)-(SELECT SUM(Purchases_td.qTy * Purchases_td.product_price) FROM Purchases_td WHERE Product_id = Products_td.Product_id) AS Profits FROM Products_td GROUP BY Product_id, Profits DESC LIMIT 1', function(err, showMostProfitableProduct) {
         if (err)
           return next("error selecting : %s", err)
         res.render('salesProfits', {
@@ -22,7 +22,7 @@ exports.searchProfitsPerProduct = function(req, res, next) {
 
   req.getConnection(function(err, connection) {
     pullProductProfit = '%' + pullProductProfit + '%';
-    var searchProductName = 'SELECT Products_td.id,Products_td.Product_name, (SELECT SUM(Sales_td.qTy * Sales_td.product_price) FROM Sales_td WHERE Product_id = Products_td.id)-(SELECT SUM(Purchases_td.qTy * Purchases_td.product_price) FROM Purchases_td WHERE Product_id = Products_td.id) AS Profits FROM Products_td WHERE Product_name LIKE ? GROUP BY Products_td.Product_name ASC';
+    var searchProductName = 'SELECT Products_td.Product_id,Products_td.Product_name, (SELECT SUM(Sales_td.qTy * Sales_td.product_price) FROM Sales_td WHERE Product_id = Products_td.Product_id)-(SELECT SUM(Purchases_td.qTy * Purchases_td.product_price) FROM Purchases_td WHERE Product_id = Products_td.Product_id) AS Profits FROM Products_td WHERE Product_name LIKE ? GROUP BY Products_td.Product_name ASC';
 
     connection.query(searchProductName, [pullProductProfit, pullProductProfit], function(err, result) {
       if (err)
