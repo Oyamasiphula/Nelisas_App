95ed19dd6d08c00bb4db484bdae6cd36b9f55d1e
oyamasiphula <oyama@projectcodex.co>
2016-09-22 14:10:10 +0200

    nothing new added

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -17,7 +17,7 @@ exports.verifyUser = function(req, res) {
 exports.checkIfAuthorized = function(req, res, next) {
   // if user is authenticated in the session, carry on
   if (req.isAuthenticated())
-    return next();
+        return next();
   // if they aren't redirect them to the home page
   res.redirect('/');
 };
